71c164dbb52b28b8bab54b150897929c45d66b92
jane <SSE35@skku.edu>
2021-09-07 13:59:16 +0900

    first commit

--- hw2.cc
+++ hw2.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
  
-int main()
+int main(int argc, char* argv[])
 {
         int temp,out1,out2,a,b,c;
-            cin >> temp;
-               
-                a = temp/100;
+            //cin >> temp;
+              temp=atoi (argv[1]); 
+               a= temp/100;
                     if(a == 1 || a == 4 || a == 7)
                     {
                                 out1= (a-1)*100+99;//최소
@@ -16,7 +16,7 @@ int main()
                                                             
                     }
                      
-                        b = (temp/10)%10;
+                     b=(temp/10)%10;
                         if(b == 1 || b == 4 || b == 7){
                                     out1= a*100+(b-1)*10+9;//최소
                                             out2= a*100+(b+1)*10;//최대
@@ -25,7 +25,7 @@ int main()
                                                                 
                         }
                          
-                            c =temp%10;
+                            c= temp%10;
                             if(c == 1 || c == 4 || c == 7){
                                         out1= a*100+b*10+c-1;
                                                 out2= a*100+b*10;
