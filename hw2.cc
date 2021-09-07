e67cf04da3019e260f0942ef7979f2839b80d374
jane <SSE35@skku.edu>
2021-09-07 13:25:38 +0900

    first commit

---
+++ hw2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if ((a != 1 && a != 4 && a != 7) && (b != 1 && b != 4 && b != 7) && (c != 1 && c != 4 && c != 7)){
+        cout<<a<<b<<c;
+    }
+}
